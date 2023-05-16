3433a3efb8f6c7d885aa7da88454c6b772f1137c
unknown <juslepi@gmail.com>
2023-05-16 22:20:28 +0300

    Init stations page

---
+++ client/app/stations/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./page.module.css";
+
+export interface Station {
+  _id: string;
+  ID: number;
+  Name: string;
+  Address: string;
+  Capacity: number;
+  x: number;
+  y: number;
+}
+
+const Page = () => {
+  const [page, setPage] = useState(1);
+  const [stations, setStations] = useState<Station[]>([]);
+
+  useEffect(() => {
+    const getStations = async () => {
+      const res = await axios.get(`http://localhost:3001/stations/${page}/10`);
+      console.log(res.data);
+      setStations(res.data);
+    };
+
+    getStations();
+  }, [page]);
+
+  return (
+    <main className={`${styles.container} center_container`}>
+      <table className={styles.journeys_table}>
+        <thead>
+          <tr>
+            <th>Station</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        {stations.map((station: Station) => (
+          <tbody className={styles.table_content} key={station._id}>
+            <tr>
+              <td>{station.Name}</td>
+              <td>{station.Address}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+      <div className={styles.toolbar}>
+        <button onClick={() => setPage((currentPage) => currentPage - 1)}>
+          {"<"}
+        </button>
+        Page: {page}
+        <button onClick={() => setPage((currentPage) => currentPage + 1)}>
+          {">"}
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
