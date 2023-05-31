5ef653f77e714452e0bcd07c040b34ba9d0d8777
unknown <juslepi@gmail.com>
2023-06-01 01:17:02 +0300

    Add toolbar component

---
+++ client/app/components/Toolbar.tsx
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction } from "react";
+import styles from "./Toolbar.module.css";
+
+type ToolbarProps = {
+  setPage: Dispatch<SetStateAction<number>>;
+  page: number;
+};
+
+const Toolbar = ({ setPage, page }: ToolbarProps) => {
+  return (
+    <div className={styles.toolbar}>
+      <button
+        onClick={() =>
+          setPage((currentPage) =>
+            currentPage > 1 ? currentPage - 1 : currentPage
+          )
+        }
+      >
+        {"<"}
+      </button>
+      Page: {page}
+      <button onClick={() => setPage((currentPage) => currentPage + 1)}>
+        {">"}
+      </button>
+    </div>
+  );
+};
+
+export default Toolbar;
